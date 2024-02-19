2bb61088526ebe0f744e29f0835c341b601efcd7
Chris Purcell <chrisp0000@gmail.com>
2024-02-18 17:20:52 -0800

    Added router for thoughts and reactions

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought');
+
+router.route('/').get(getAllThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router
